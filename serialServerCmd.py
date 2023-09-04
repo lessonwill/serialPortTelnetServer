4d79e1af4e1330774d8780e4453f16713c8b21f0
lessonwill <132534021+lessonwill@users.noreply.github.com>
2023-09-04 17:34:54 +0800

    seperate GUI mode from command mode -- part1

--- serialServerCmd.py
+++ serialServerCmd.py
@@ -21,15 +21,11 @@ telnet_handshake =  IAC + WILL + SGA + IAC + WILL + ECHO + IAC + WILL + BINARY
 clients = []  # List to keep track of connected clients
 
 def find_first_ip():
-    if platform.system() == 'Linux':
-        # 获取 IP 地址的 Linux 版本代码
-        return None
-    elif platform.system() == 'Windows':
-        # Windows 版本的代码
-        for interface, addrs in psutil.net_if_addrs().items():
-            for addr in addrs:
-                if addr.family == socket.AF_INET:
-                    return addr.address
+#    if platform.system() == 'Linux':
+    for interface, addrs in psutil.net_if_addrs().items():
+        for addr in addrs:
+            if addr.family == socket.AF_INET and not addr.address.startswith('127.'):
+                return addr.address
     return None
 
 class SerialToTelnetProtocol(asyncio.Protocol):
@@ -114,81 +110,6 @@ async def stop_telnet_server():
         await writer.wait_closed()
     clients.clear()
 
-import tkinter as tk
-from tkinter import ttk
-import asyncio
-import serial
-import serial.tools.list_ports
-import threading
-
-class TelnetServerApp:
-    def __init__(self, master):
-        self.server = None
-        self.read_serial_task = None
-        self.serial_transport = None
-
-        self.master = master
-        self.master.title("Telnet Server")
-
-        frame = ttk.Frame(self.master, padding="10")
-        frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
-
-        # COM port dropdown
-        ttk.Label(frame, text="COM Port:").grid(row=0, column=0, sticky=tk.W)
-        self.com_port_dropdown = ttk.Combobox(frame)
-        self.com_port_dropdown.grid(row=0, column=1, sticky=(tk.W, tk.E))
-        self.populate_com_ports()
-
-        # Baud rate entry
-        ttk.Label(frame, text="Baud Rate:").grid(row=1, column=0, sticky=tk.W)
-        self.baud_rate_entry = ttk.Entry(frame)
-        self.baud_rate_entry.grid(row=1, column=1, sticky=(tk.W, tk.E))
-        self.baud_rate_entry.insert(0, "9600")
-
-        # Telnet port entry
-        ttk.Label(frame, text="Telnet Port:").grid(row=2, column=0, sticky=tk.W)
-        self.port_entry = ttk.Entry(frame)
-        self.port_entry.grid(row=2, column=1, sticky=(tk.W, tk.E))
-        self.port_entry.insert(0, "9001")
-
-        # Connect button
-        self.connect_button = ttk.Button(frame, text="Connect", command=self.on_connect_disconnect)
-        self.connect_button.grid(row=3, columnspan=2)
-
-        self.bConnected = False
-        self.loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(self.loop)
-
-    # Function to populate the COM port dropdown
-    def populate_com_ports(self):
-        com_ports = [port.device for port in serial.tools.list_ports.comports()]
-        self.com_port_dropdown['values'] = com_ports
-        if com_ports:
-            self.com_port_dropdown.current(0)
-
-    # Function to update the global baud rate and Telnet port
-    def update_baud_rate_and_port(self):
-        try:
-            self.baud_rate = int(self.baud_rate_entry.get())
-            self.telnet_port = int(self.port_entry.get())
-        except ValueError:
-            print("Invalid input. Using default values for baud rate (9600) and Telnet port (9001).")
-
-    def on_connect_disconnect(self):
-        if not self.bConnected:
-            self.update_baud_rate_and_port()
-            selected_port = self.com_port_dropdown.get()
-            if not selected_port:
-                print("No COM port selected.")
-                return
-            asyncio.run_coroutine_threadsafe(start_telnet_server(selected_port, self.baud_rate, self.telnet_port), self.loop)
-            self.connect_button.config(text="Disconnect")
-            self.bConnected = True
-        else:
-            asyncio.run_coroutine_threadsafe(stop_telnet_server(), self.loop)
-            self.connect_button.config(text="Connect")
-            self.bConnected = False
-
 def run_event_loop(app):
     app.loop.run_forever()
 
@@ -210,8 +131,11 @@ if __name__ == '__main__':
         else:
             print("In CLI mode, you must specify --com, --baud, and --port.")
     else:
+        import tkinter as tk
+        import threading
+        import serialServerGui
         root = tk.Tk()
-        app = TelnetServerApp(root)
+        app = serialServerGui.TelnetServerApp(root)
         thread = threading.Thread(target=run_event_loop, args=(app,))
         thread.start()
         root.mainloop()
